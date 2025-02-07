ee0a81f309a623dbca907c9ce13aea02873ad429
Daniel Fernando da Silva de Castro <daniel.fs.castro@gmail.com>
2025-02-07 20:36:49 -0300

    Update KeycloakRoleConverter.java
    unchecked conversions problem

--- apigateway/src/main/java/com/fakepay/apigateway/security/KeycloakRoleConverter.java
+++ apigateway/src/main/java/com/fakepay/apigateway/security/KeycloakRoleConverter.java
@@ -1,14 +1,12 @@
 package com.fakepay.apigateway.security;
 
+import com.nimbusds.jose.shaded.gson.Gson;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class KeycloakRoleConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
@@ -20,14 +18,19 @@ public class KeycloakRoleConverter implements Converter<Jwt, Collection<GrantedA
      * @return A collection of GrantedAuthority objects representing the user's roles.
      */
     public Collection<GrantedAuthority> convert(Jwt source) {
-        Map<String, Object> realmAccess = (Map<String, Object>) source.getClaims().get("realm_access");
+        Gson gson = new Gson();
+        Map<String, Object> realmAccess = gson.fromJson(source.getClaims().get("realm_access").toString(), Map.class);
+
         if (realmAccess == null || realmAccess.isEmpty()) {
-            return new ArrayList<>();
+            return Collections.emptyList();
         }
-        Collection<GrantedAuthority> returnValue = ((List<String>) realmAccess.get("roles"))
-                .stream().map(roleName -> "ROLE_" + roleName)
+
+        String rolesString = realmAccess.get("roles").toString();
+        List<String> roles = Arrays.asList(rolesString.replaceAll("\\[|\\]", "").split(", "));
+
+        return roles.stream()
+                .map(roleName -> "ROLE_" + roleName)
                 .map(SimpleGrantedAuthority::new)
                 .collect(Collectors.toList());
-        return returnValue;
     }
 }
