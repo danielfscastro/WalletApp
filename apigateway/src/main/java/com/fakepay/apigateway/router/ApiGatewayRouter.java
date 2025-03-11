b4b97a6e9f75d1bb7651b34768e8d0ddd1e53b4f
Daniel Fernando da Silva de Castro <daniel.fs.castro@gmail.com>
2025-03-11 18:04:50 -0300

    config rate limiter and keycloak

--- apigateway/src/main/java/com/fakepay/apigateway/router/ApiGatewayRouter.java
+++ apigateway/src/main/java/com/fakepay/apigateway/router/ApiGatewayRouter.java
@@ -2,6 +2,8 @@ package com.fakepay.apigateway.router;
 
 import com.fakepay.apigateway.filters.ResponseTimeHeaderFilter;
 import lombok.AllArgsConstructor;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -13,22 +15,28 @@ public class ApiGatewayRouter {
 
     private ResponseTimeHeaderFilter responseTimeHeaderFilter;
 
+    private RedisRateLimiter redisRateLimiter;
+
+    private KeyResolver userKeyResolver;
+
     @Bean
     public RouteLocator routeConfig(RouteLocatorBuilder routeLocatorBuilder){
 
         return routeLocatorBuilder.routes()
                 .route(
-                    route -> route.path("/fakepay/customer/**")
-                            .filters(filter -> filter.rewritePath("/fakepay/customer/(?<segment>.*)", "/${segment}")
-                                    .filter(responseTimeHeaderFilter)
-                                    .circuitBreaker(config -> config.setName("customerCircuitBreaker")
-                                            .setFallbackUri("forward:/contactSupport")))
-                            .uri("lb://CUSTOMER")
+                        route -> route.path("/fakepay/customer/**")
+                                .filters(filter -> filter.rewritePath("/fakepay/customer/(?<segment>.*)", "/${segment}")
+                                        .filter(responseTimeHeaderFilter)
+                                        .requestRateLimiter(config -> config.setRateLimiter(redisRateLimiter).setKeyResolver(userKeyResolver))
+                                        .circuitBreaker(config -> config.setName("customerCircuitBreaker")
+                                                .setFallbackUri("forward:/contactSupport")))
+                                .uri("lb://CUSTOMER")
                 )
                 .route(
                         route -> route.path("/fakepay/wallet/**")
                                 .filters(filter -> filter.rewritePath("/fakepay/wallet/(?<segment>.*)", "/${segment}")
                                         .filter(responseTimeHeaderFilter)
+                                        .requestRateLimiter(config -> config.setRateLimiter(redisRateLimiter).setKeyResolver(userKeyResolver))
                                         .circuitBreaker(config -> config.setName("walletCircuitBreaker")
                                                 .setFallbackUri("forward:/contactSupport")))
                                 .uri("lb://WALLET")
--- apigateway/src/main/java/com/fakepay/apigateway/security/GatewayAuthHttpRequestConfigurer.java
+++ apigateway/src/main/java/com/fakepay/apigateway/security/GatewayAuthHttpRequestConfigurer.java
@@ -6,13 +6,12 @@ public final class GatewayAuthHttpRequestConfigurer {
 
     public static void configure(ServerHttpSecurity http) throws Exception {
         http.authorizeExchange((requests) -> requests
-                        .pathMatchers("/fakepay/customer/**", "/fakepay/wallet/**").hasAnyRole("USER", "ADMIN")
-                        .pathMatchers("/fakepay/admin/**").hasRole("ADMIN")
-                        .pathMatchers("/fakepay/user").authenticated()
-                        .pathMatchers("/fakepay/contact", "/fakepay/home", "/fakepay/error", "/fakepay/register").permitAll()
-                        .anyExchange().authenticated()
-                        )
-                        ;
-
+                .pathMatchers("/fakepay/customer/**", "/fakepay/wallet/**").hasAnyRole("USER", "ADMIN")
+                .pathMatchers("/fakepay/admin/**").hasRole("ADMIN")
+                .pathMatchers("/fakepay/user").authenticated()
+                .pathMatchers("/fakepay/contactSupport", "/fakepay/error" ,"/actuator/**").permitAll()
+                .anyExchange().authenticated()
+        )
+        ;
     }
 }
